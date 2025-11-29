60a23dff1fc3ca6c02293b9c687048616d423833
EricJoel-Code <joeld214254@gmail.com>
2025-11-29 16:14:37 -0500

    update App.jsx, pending setError and update with-results.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
 import './App.css'
+//import { useRef } from 'react'
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
+import { useEffect, useState } from 'react'
 
 function App() {
   const { movies: mappedMovies } = useMovies()
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ query })
+  }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    if (newQuery.startsWith(' ')) return
+    setQuery(newQuery)
+  }
+
+  useEffect(() => {
+    if (query == '') {
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+    if (query.length < 3) {
+      setError('La busqueda debe tener al menos 3 caracteres')
+      return
+    }
+    setError(null)
+  }, [query])
+
 
   return (
     <div className='page'>
 
       <header>
         <h1>Buscador de Peliculas</h1>
-        <form action="" className='Form'>
-          <input placeholder='The Matrix, Saving Private Ryan, Lone Survivor .....' />
+        <form className='Form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={query} name='query' placeholder='The Matrix, Saving Private Ryan, Lone Survivor .....' />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
